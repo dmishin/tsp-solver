ad95d67f7d5586f39dc3354ff5e4702e7ef9ae17
Dmitry Shintyakov <shintyakov@gmail.com>
2017-09-10 13:47:36 +0300

    setup: better setup for PyPi

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setup(name='tsp_solver',
       author_email='shintyakov@gmail.com',
       url='https://github.com/dmishin/tsp-solver',
       packages=['tsp_solver'],
+      keywords=['travelling salesman problem', 'optimization'],
       scripts=['bin/tsp_demo', 'bin/tsp_numpy2svg']
       )
