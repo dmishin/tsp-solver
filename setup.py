cf7ec7bd726375e8ff9d48ac89d12f7d4f825fc1
Dmitry Shintyakov <shintyakov@gmail.com>
2017-09-10 14:37:01 +0300

    minor: typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='tsp_solver',
       version='0.2',
-      description='Greedy, supoptimal solver for the Travelling Salesman Problem',
+      description='Greedy, suboptimal solver for the Travelling Salesman Problem',
       author='Dmitry Shintyakov',
       author_email='shintyakov@gmail.com',
       url='https://github.com/dmishin/tsp-solver',
