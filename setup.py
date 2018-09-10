99cee89732e06a1dfb81ea5a6f4a82f295a97804
Erik Jansson <erikjansson90@gmail.com>
2018-09-10 08:41:54 +0200

    Add long_description option to setup()

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='tsp_solver2',
       version='0.3',
       description='Greedy, suboptimal solver for the Travelling Salesman Problem',
       author='Dmitry Shintyakov',
       author_email='shintyakov@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/dmishin/tsp-solver',
       packages=['tsp_solver', 'tsp_solver.demo'],
       keywords=['travelling salesman problem', 'optimization'],
